645d0d8121f1df1cace33afb4770141b162c31d8
olasrum <mursalo@gmail.com>
2022-07-21 17:00:29 +1000

    Done with assignment

---
+++ index.js
@@ -0,0 +1,30 @@
+function shout(string) {
+    return string.toUpperCase();
+}
+
+function whisper(string) {
+    return string.toLowerCase();
+}
+
+function logShout(string) {
+     console.log(string.toUpperCase());
+}
+
+function logWhisper(string) {
+    console.log(string.toLowerCase());
+}
+
+function sayHiToHeadphonedRoommate (string) {
+    var cantHear = "I can't hear you!";
+    var yesIndeed = "YES INDEED!";
+    var lovToo = "I would love to!";
+    if (string.toLowerCase(string) === string) {
+      return cantHear;
+    }
+    else if (string.toUpperCase(string) === string) {
+      return yesIndeed;
+    }
+    else if ("Let's have dinner together!" === string) {
+      return lovToo
+    }
+  }
